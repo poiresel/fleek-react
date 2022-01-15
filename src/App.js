38864ba827b8c6c6e239bbb0ed1d715f1d662807
poiresel <deepa.s.sathaye@gmail.com>
2022-01-15 18:52:49 -0500

    commit in base react project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        This is my test fleek react project!
+      </header>
+    </div>
+  );
+}
+
+export default App;
